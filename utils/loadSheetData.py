19ffc222bbf88d56cb371a8e67711fc23c74a324
Harsh1s <thisishraj@gmail.com>
2023-12-07 02:14:41 +0530

    Feat: Add function to get data from Excel sheets

--- utils/loadSheetData.py
+++ utils/loadSheetData.py
@@ -1,3 +1,21 @@
-from pandas import read_excel
+import pandas as pd
+import os
 
-#TODO: Import data from excel sheet for processing
+absolutePath = os.path.dirname(__file__)
+dataDirectory = 'data'
+
+scheduleFileName = 'schedule.xlsx'
+bookingPNRFileName = 'bookingPNR.xlsx'
+passengerPNRFileName = 'passengerPNR.xlsx'
+seatAvailabilityFileName = 'seatAvailability.xlsx'
+
+
+scheduleFilePath = os.path.join(absolutePath, '..', dataDirectory, scheduleFileName)
+bookingPNRFilePath = os.path.join(absolutePath, '..', dataDirectory, bookingPNRFileName)
+passengerPNRFilePath = os.path.join(absolutePath, '..', dataDirectory, passengerPNRFileName)
+seatAvailabilityFilePath = os.path.join(absolutePath, '..', dataDirectory, seatAvailabilityFileName)
+
+scheduleData = pd.read_excel(scheduleFilePath)
+bookingPNRData = pd.read_excel(bookingPNRFilePath)
+passengerPNRData = pd.read_excel(passengerPNRFilePath)
+seatAvailabilityData = pd.read_excel(seatAvailabilityFilePath)
