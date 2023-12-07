42d33e26b4a70d5593feaddb30b48778ae7ceaea
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-12-07 23:14:01 +0530

    Fix case

--- models/pnrPassenger.py
+++ models/pnrPassenger.py
@@ -1,14 +1,14 @@
 class PNRPassenger:
-    def __init__(self, recloc, creation_dtz, last_name, first_name, nationality, contact_ph_num, contact_email, doc_id, doc_type, special_name_cd1, special_name_cd2, ssr_code_cd1):
+    def __init__(self, recloc, creationDate, lastName, firstName, nationality, contactNo, contact_email, docID, docType, specialNameCode1, specialNameCode2, ssr):
         self.recloc = recloc
-        self.creation_dtz = creation_dtz
-        self.last_name = last_name
-        self.first_name = first_name
+        self.creationDate = creationDate
+        self.lastName = lastName
+        self.firstName = firstName
         self.nationality = nationality
-        self.contact_ph_num = contact_ph_num
+        self.contactNo = contactNo
         self.contact_email = contact_email
-        self.doc_id = doc_id
-        self.doc_type = doc_type
-        self.special_name_cd1 = special_name_cd1
-        self.special_name_cd2 = special_name_cd2
-        self.ssr_code_cd1 = ssr_code_cd1
+        self.docID = docID
+        self.docType = docType
+        self.specialNameCode1 = specialNameCode1
+        self.specialNameCode2 = specialNameCode2
+        self.ssr = ssr
