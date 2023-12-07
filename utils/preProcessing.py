50d5804b98764f34f4d105e8cb80cb395164cc73
Harsh1s <thisishraj@gmail.com>
2023-12-07 23:18:51 +0530

    Feat: Add minDownTime argument to function getPossibleRoutes

--- utils/preProcessing.py
+++ utils/preProcessing.py
@@ -5,93 +5,93 @@ from loadSheetData import scheduleDataObjects, bookingPNRDataObjects
 import pandas as pd
 
 
-def get_seconds(t):
-  return (t.hour * 60 + t.minute) * 60 + t.second
+def getSeconds(t):
+    return (t.hour * 60 + t.minute) * 60 + t.second
 
-# changing dates and times to epochs
-# for schedule_file->
+# Changing dates and times to epochs
+# For schedule_file ->
 for schedule in scheduleDataObjects:
-  date_list_string = schedule.departureDates[1:-1]
-  date_list = date_list_string.split(',')
-  datetime_obj_list = []
-  for date_string in date_list:
-    date_string = date_string.strip()
-    if date_string != '':
-      datetime_obj_list.append(datetime.strptime(date_string[1:-1], '%m/%d/%Y'))
-  schedule.departureDateTimes = datetime_obj_list
+    dateListString = schedule.departureDates[1:-1]
+    dateList = dateListString.split(',')
+    dateTimeObjList = []
+    for dateString in dateList:
+        dateString = dateString.strip()
+        if dateString != '':
+            dateTimeObjList.append(datetime.strptime(dateString[1:-1], '%m/%d/%Y'))
+    schedule.departureDateTimes = dateTimeObjList
 
 for schedule in scheduleDataObjects:
-  list_epoch_dates = []
-  for dates in schedule.departureDateTimes:
-    temp_datetime_obj = datetime.combine(dates,schedule.departureTime)
-    tmp_epoch = calendar.timegm(temp_datetime_obj.timetuple())
-    list_epoch_dates.append(tmp_epoch)
-  schedule.departureEpochs = list_epoch_dates
-  schedule.duration = ((get_seconds(schedule.arrivalTime)-get_seconds(schedule.departureTime) + 86400) % 86400)
+    listEpochDates = []
+    for dates in schedule.departureDateTimes:
+        tempDateTimeObj = datetime.combine(dates, schedule.departureTime)
+        tmpEpoch = calendar.timegm(tempDateTimeObj.timetuple())
+        listEpochDates.append(tmpEpoch)
+    schedule.departureEpochs = listEpochDates
+    schedule.duration = ((getSeconds(schedule.arrivalTime) - getSeconds(schedule.departureTime) + 86400) % 86400)
 
-#getting the data for passengers with cancelled flights
-# cancelled_flights = schedule_file[schedule_file['Status'] == 'Cancelled']
-cancelled_flights = []
+# Getting the data for passengers with cancelled flights
+# cancelledFlights = schedule_file[schedule_file['Status'] == 'Cancelled']
+cancelledFlights = []
 for schedule in scheduleDataObjects:
-  if schedule.status == 'Cancelled':
-    cancelled_flights.append(schedule)
+    if schedule.status == 'Cancelled':
+        cancelledFlights.append(schedule)
 
-list_of_cancel_flightnum = []
+listOfCancelFlightNum = []
 
-for flight in cancelled_flights:
-  list_of_cancel_flightnum.append(flight.flightNo)
+for flight in cancelledFlights:
+    listOfCancelFlightNum.append(flight.flightNo)
 
 for booking in bookingPNRDataObjects:
-  booking.departureDTMZEpoch = calendar.timegm((booking.departureDTMZ).timetuple())
+    booking.departureDTMZEpoch = calendar.timegm((booking.departureDTMZ).timetuple())
 
-# filtering out the bookings that were cancelled
+# Filtering out the bookings that were cancelled
 # Returns a list of reclocs by comparing flight number and departure epochs
-def get_affected_passengers(scheduleID,departureDate):
-  bookings_cancelled = []
-  cancelled_flight_dep_epoch = 0
-  for flight in scheduleDataObjects:
-      if flight.scheduleID == scheduleID:
-        index = 0
-        for i in range(len(flight.departureDateTimes)):
-           if flight.departureDateTimes[i] == departureDate:
-              index = i
-              break
-      cancelled_flight_dep_epoch = flight.departureEpochs[index]
-  for booking in bookingPNRDataObjects:
-     if booking.departureDTMZEpoch == cancelled_flight_dep_epoch:
-        bookings_cancelled.append(booking.recloc)
-  return bookings_cancelled
+def getAffectedPassengers(scheduleID, departureDate):
+    bookingsCancelled = []
+    cancelledFlightDepEpoch = 0
+    for flight in scheduleDataObjects:
+        if flight.scheduleID == scheduleID:
+            index = 0
+            for i in range(len(flight.departureDateTimes)):
+                if flight.departureDateTimes[i] == departureDate:
+                    index = i
+                    break
+            cancelledFlightDepEpoch = flight.departureEpochs[index]
+    for booking in bookingPNRDataObjects:
+        if booking.departureDTMZEpoch == cancelledFlightDepEpoch:
+            bookingsCancelled.append(booking.recloc)
+    return bookingsCancelled
 
 
-def get_possible_routes(dataset, k, x, start_airport, end_airport, start_datetime_epoch, max_end_datetime_epoch):
-    def find_routes(route, current_airport, current_time):
+def getPossibleRoutes(dataset, maxConnectingFlights, maxDownTime, startAirport, endAirport, startDatetimeEpoch, maxEndDatetimeEpoch, minDownTime):
+    def findRoutes(route, currentAirport, currentTime):
         nonlocal result
 
         # Check if the current route exceeds the allowed time
-        if current_time > max_end_datetime_epoch:
+        if currentTime > maxEndDatetimeEpoch:
             return
 
         # Check if the current airport is the destination
-        if current_airport == end_airport:
+        if currentAirport == endAirport:
             result.append(route.copy())
             return
 
         # Find possible connecting flights
-        possible_flights = [flight for flight in dataset if flight[1] == current_airport and flight[3] >= current_time]
+        possibleFlights = [flight for flight in dataset if flight[1] == currentAirport and flight[3] >= currentTime]
 
-        for i in range(min(k, len(possible_flights))):
-            next_flight = possible_flights[i]
-            next_departure_time = next_flight[3] + next_flight[4]  # Departure time + Duration
+        for i in range(min(maxConnectingFlights, len(possibleFlights))):
+            nextFlight = possibleFlights[i]
+            nextDepartureTime = nextFlight[3] + nextFlight[4]  # Departure time + Duration
 
             # Check if the downtime at the current stop is within limits
-            if next_departure_time - current_time <= x:
-                find_routes(route + [next_flight[0]], next_flight[2], next_departure_time)
+            if nextDepartureTime - currentTime <= maxDownTime and nextDepartureTime - currentTime >= minDownTime:
+                findRoutes(route + [nextFlight[0]], nextFlight[2], nextDepartureTime)
 
     result = []
-    start_flights = [flight for flight in dataset if flight[1] == start_airport and flight[3] >= start_datetime_epoch]
+    startFlights = [flight for flight in dataset if flight[1] == startAirport and flight[3] >= startDatetimeEpoch]
 
-    for start_flight in start_flights:
-        find_routes([start_flight[0]], start_flight[2], start_flight[3] + start_flight[4])
+    for startFlight in startFlights:
+        findRoutes([startFlight[0]], startFlight[2], startFlight[3] + startFlight[4])
 
     return result
 
@@ -106,9 +106,10 @@ for schedule in scheduleDataObjects:
 
 
 # Example usage:
-start_datetime = calendar.timegm(datetime(2023,12,14,0,0,0).timetuple())
-start_airport = 'CNN'
-end_airport = 'MAA'
-result = get_possible_routes(dataset, k = 3, x = 30 * 3600, start_airport=start_airport, end_airport=end_airport, start_datetime_epoch=start_datetime, max_end_datetime_epoch = 100 * 3600 + start_datetime)
+startDatetime = calendar.timegm(datetime(2023, 12, 14, 0, 0, 0).timetuple())
+startAirport = 'CNN'
+endAirport = 'MAA'
+result = getPossibleRoutes(dataset, maxConnectingFlights=3, maxDownTime=30 * 3600, startAirport=startAirport,
+                            endAirport=endAirport, startDatetimeEpoch=startDatetime,
+                            maxEndDatetimeEpoch=100 * 3600 + startDatetime)
 result
-
