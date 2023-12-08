62fecc1e3a2e44ca867d834eb4112d88f381da55
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-12-08 20:39:33 +0530

    add flight endpoints

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
+from endpoints.availableFlights import AvailableFlights
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class Home(Resource):
@@ -9,6 +12,7 @@ class Home(Resource):
         return {'status': 'The API is running and healthy!'}
 
 api.add_resource(Home, '/')
+api.add_resource(AvailableFlights, '/flights')
 
 if __name__ == '__main__':
     app.run(debug=True)
